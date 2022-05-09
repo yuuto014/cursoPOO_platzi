92f157944a8596a22dbdc5e4ebc389e82f300948
yuuto <larrota.juancamilo@gmail.com>
2022-05-09 15:44:54 -0500

    implementacion de constructor en Java

--- Java/Account.java
+++ Java/Account.java
@@ -6,4 +6,8 @@ class Account{
     String email;
     String password;
     
+    public Account(String name, String document){
+        this.name = name;
+        this.document = document;
+    }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,15 +1,11 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("hola mundo");
-        Car car = new Car();
-        car.license = "AMQ342";
-        car.driver = "Andres herrrera";
+        Car car = new Car( "AMQ342",new Account("Andres Herrera", "1013254323"));
         car.passegenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "ACR902";
-        car2.driver = "Camila herrrera";
+        Car car2 = new Car("ACR902",new Account("Camila Herrera","100532659"));
         car2.passegenger = 3;
         car2.printDataCar();
     }
