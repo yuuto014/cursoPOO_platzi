13557e683932bc4e1f9fb54fbd7e39e840dd8ef5
yuuto <larrota.juancamilo@gmail.com>
2022-05-09 15:46:42 -0500

    implementacion de clases y construcor en JS, con el uso de prototipos

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+var car = new Car("AWS342",new Account("Andres Herrera","1013254323"));
+car.passager = 4;
+car.printDataCar();
+var car2 = new Car("ACR902",new Account("Camila Herrera","100532659"));
+car2.passager = 3;
+car2.printDataCar();
