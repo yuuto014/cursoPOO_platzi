4a77f6be4980f83cb608ff5ab28ec88cd40507c6
yuuto <larrota.juancamilo@gmail.com>
2022-05-09 13:38:24 -0500

    Ejemplo de objeto en Java

--- Java/Car.java
+++ Java/Car.java
@@ -4,4 +4,8 @@ class Car{
     String driver;
     Integer passegenger;
 
+    void printDataCar(){
+        System.out.println("license: "+license+" Driver: "+driver);
+    }
+
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,16 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        Car car = new Car();
+        car.license = "AMQ342";
+        car.driver = "Andres herrrera";
+        car.passegenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "ACR902";
+        car2.driver = "Camila herrrera";
+        car2.passegenger = 3;
+        car2.printDataCar();
     }
 }
