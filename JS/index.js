8ad664271062419f9316bb3c200e80b5be0b367e
yuuto <larrota.juancamilo@gmail.com>
2022-05-16 09:42:31 -0500

    herencia en JavaScript

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,9 @@
 var car = new Car("AWS342",new Account("Andres Herrera","1013254323"));
 car.passager = 4;
 car.printDataCar();
-var car2 = new Car("ACR902",new Account("Camila Herrera","100532659"));
-car2.passager = 3;
-car2.printDataCar();
+// var car2 = new Car("ACR902",new Account("Camila Herrera","100532659"));
+// car2.passager = 3;
+// car2.printDataCar();
+var uberX = new UberX("SEB643",new Account("Pedro Alcacerez","1003654783"),"Toyota","Prado");
+uberX.passager = 5;
+uberX.printDataCar();
