822b24731eb16ca7c881eb0b8d055ed112817277
yuuto <larrota.juancamilo@gmail.com>
2022-05-16 09:09:57 -0500

    PHP uso de herencia y creacion de objetos

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,17 @@
 <?php
 require_once("car.php");
 require_once("account.php");
+require_once("uberX");
+require_once("uberPool");
 
-$car = new Car("ASE264",new Account("Camila Rojas","1034562342"));
-$car->printDataCar();
+$uberX new UberX("CVB134",new Account("Andres Herrera","1025462674"),"Chevrolet","Spark");
+$uberX->printDataCar();
+
+$uberPool = new UberPool("CVB134",new Account("Andrea Ferrant","1023744589"),"Chevrolet","Spark");
+$uberPool ->printDataCar();
+
+
+// $car = new Car("ASE264",new Account("Camila Rojas","1034562342"));
+// $car->printDataCar();
+
 ?>
